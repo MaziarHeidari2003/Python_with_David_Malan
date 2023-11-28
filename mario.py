3898c1715174acae730b80117afebc10013d003c
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-11-28 13:06:43 -0800

    commited

--- mario.py
+++ mario.py
@@ -5,14 +5,16 @@ def print_column(height):
     print("#\n"* height , end="")  
 main()
 """
-"""
+
 def print_square(size):
   for i in range(size):
     for j in range(size):
       print("#",end="")
     print()  
 
-print_square(4)    
+print_square(4)   
+ 
 """
 for i in range(5):
   print("#" * 5)
+  """
