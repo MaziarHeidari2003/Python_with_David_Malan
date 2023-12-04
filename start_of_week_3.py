32922692322b8df443219cd42df33ef0672cabc4
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-04 07:06:15 -0800

    end of the error handling session

--- start_of_week_3.py
+++ start_of_week_3.py
@@ -1,34 +1,15 @@
-"""
-try:
-  x = int(input("Enter: "))
-except ValueError:
-  print("x is not an integer")
-else:  
-  print(f"x is {x}")    
-"""
-"""
-
-while True:
-  try: 
-    x = int(input("Enter: "))
-  except ValueError:
-    print("x is not an integer")
-  else:
-    break
-print(f"x is {x}")  
-"""
 def main():
-  x= get_int()
+  x = get_int("whats x? ")
   print(f"x is {x}")
 
-
-def get_int():
+def get_int(prompt):
   while True:
     try:
-      #return int(input("Enter"))
-      x= int(input("Enter: "))
-      return x
+      return int(input(prompt))
     except ValueError:
       print("x is not an integer")
 
+
 main()
+
+  
