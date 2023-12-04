b1607c6a6a463bb25ee1fe1a8e70c099b24f4319
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-04 10:18:11 -0800

    practicing dictss

---
+++ problemset3_1.py
@@ -0,0 +1,22 @@
+student = {'name':'Jhon', 'age':25, 'courses':['Math','ComSci']}
+student['phone'] = '555-5555'
+print(student['name'])
+print(student.get('name','not found'))
+
+student.update({'name': 'jan', 'age':24, })
+
+age = student.pop('age')
+print(student)
+print(age)
+
+menu = {
+  "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
